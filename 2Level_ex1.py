83aa16a2c2790c84eea542a43f6bc14b03cca030
Erik Alvarez <erikfilias@gmail.com>
2019-10-02 17:28:05 +0200

    Create 2Level_ex1.py

---
+++ 2Level_ex1.py
@@ -0,0 +1,14 @@
+from pyomo.environ import *
+from pyomo.bilevel import *
+
+M = ConcreteModel()
+M.x = Var(bounds=(0,None))
+M.y = Var(bounds=(0,None))
+M.o = Objective(expr=M.x - 4*M.y)
+
+M.sub = SubModel(fixed=M.x)
+M.sub.o = Objective(expr=M.y)
+M.sub.c1 = Constraint(expr=- M.x - M.y <= -3)
+M.sub.c2 = Constraint(expr=-2*M.x + M.y <= 0)
+M.sub.c3 = Constraint(expr= 2*M.x + M.y <= 12)
+M.sub.c4 = Constraint(expr=-3*M.x + 2*M.y <= -4)
